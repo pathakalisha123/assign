08a90403fdcb8d4f427965e8c12b68538cc6ff74
pathakalisha123 <pathakalisha123@outlook.com>
2019-11-07 17:27:32 -0800

    first commit

---
+++ intro.py
@@ -0,0 +1,178 @@
+# x=10
+# print(f' the number is {x}')
+
+# name='admin'
+# age=20
+# print(f'your name is{name} the age is{age}')
+
+# class='bex'
+# roll=7
+# print(f'your faculty is{class} roll no is{roll}')
+
+# name='admin'
+# print(type(name))
+# age=40
+# print(type(age))
+# is is not in not in
+# x=10
+# y=20
+# z=x
+# print(x is y)
+# print(x is z)
+# print(x is not y)
+# print(y is x)
+# print(x+y)
+# print(x*y)
+# print(x-y)
+# print(x%y)
+# print(x**y)
+# print(x/y)
+
+# name='alisha pathak'
+# print(dir(name))
+# print(name.title())
+
+# python data types
+# integer int float complex
+# string
+# boolean
+# sequence type list tuple dic set
+#
+# x=input('enter x')
+# y=input('enter y')
+
+# as,and,break,class,continue,def,del,else,elif,except,global,in,import,if
+# pass,or,not,yield,with,while,for,from,finally
+# return,try,True,False,None,assert,lamda,nonlocal,raise
+
+# x=int(input('enter x '))
+# y=int(input('enter y '))
+# z=x+y
+# print(f'the sum is{z})
+
+
+# == > < >= <= !=
+
+# x=10
+# y=20
+# if x > y:
+#     print("x is large")
+# else:
+#     print("y is large")
+
+# print(x==y)
+
+# username = 'admin'
+# password = 'admin002'
+# if username == 'admin':
+#     if password == 'admin002':
+#         print('welcome admin')
+#     else:
+#         print(' incorrect')
+# else:
+#     print('incorrect')
+
+# username='alisha'
+# password=1234
+# if username== 'alisha' and password==1234:
+#     print(f'welcome {username}')
+# else:
+#     print('incorrect')
+
+# x=10
+# y=20
+# z=30
+# if x>y and x>z:
+#     print('x is greater')
+# elif y>x and y>z:
+#     print('y is greater')
+# else:
+#     print('z is greater')
+
+#hw take input of diff subjects
+#nep,eng,math,sci,pop
+#tot,pre,div,pas or fail
+
+#qt=3 delivery-home,pickup  packing; plastic500,gitbox 2000,bag1000  location ma add price 13per if kathmandu
+#total amt,tax amt,grand total,price laptop dini
+# dell = int((input'enter quantity'))
+#
+# dely = input('select home/pickup')
+# if dely == 'home':
+#     price = 1000
+
+
+math = int(input('enter math marks:'))
+sci = int(input('enter science marks:'))
+acc = int(input('enter account marks:'))
+eng = int(input('enter eng marks:'))
+nep = int(input('enter nepali marks:'))
+total = math + sci + acc + eng + nep
+print(f'the total marks is {total}')
+per = total / 5
+print(f'percentage is {per}')
+if per >= 35 and per <= 44:
+    print('third division')
+elif per >= 45 and per <= 59:
+    print('second division')
+elif per >= 60 and per <= 79:
+    print('first division')
+elif per >= 80:
+    print('distinction')
+else:
+    print('FAIL')
+
+if math < 50:
+    print('fail in math ')
+if sci < 50:
+    print('fail in science')
+if acc < 50:
+    print('fail in account')
+if eng < 50:
+    print('fail in english ')
+if nep < 50:
+    print('fail in nepali ')
+
+
+
+
+
+# dell=30000
+# acer=40000
+# mac=50000
+# dell1=int(input('enter dell quanity'))
+# acer1=int(input('enter acer quanity'))
+# mac1=int(input('enter mac quanity'))
+# total=(dell*dell1)+(acer*acer1)+(mac*mac1)
+# print(f'total {total}')
+# delivery=input('select home / pickup')
+#
+# if delivery=='home':
+#     price=1000
+#     dely=total+price
+#     print(f'price including delivery charge {dely}')
+# else:
+#     price=0
+#     dely = total + price
+#     print(f'the price is {dely}')
+# plastic=500
+# gitbox=2000
+# bag=1000
+# packing=plastic+gitbox+bag
+# pprice=((dell1+acer1+mac1)*packing)+dely
+# print(f'the price including packing is {pprice}')
+# address=input('select city ktm,pkr,ctw')
+# if address == 'ktm':
+#     tax = pprice * 0.13
+#     print(f'the tax is {tax}')
+# elif address =='pkr':
+#     tax = pprice * .10
+#     print(f'the tax is {tax}')
+# else:
+#     tax = pprice * .10
+#     print(f'the tax is{tax}')
+# grand=pprice+tax
+# print(f'the grand price to be paid is {grand}')
+# print('thanks for purchasing')
+
+
